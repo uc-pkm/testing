6603c51578192e31315da23cc96a5ee56161dd4e
Prem <Prem@PremKumar>
2014-02-11 10:58:30 +0530

    Revert "new commit"
    This reverts commit 02f3bde68c9bc8f637062587b73283aaa6c1bd1e.

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
 print_r(phpinfo());
 echo "testing";
-echo "testing1";
-
-print_r(phpinfo());
-echo "testing";
-echo "testing1";
-
-print_r(phpinfo());
-echo "testing";
-echo "testing1";
 ?>
