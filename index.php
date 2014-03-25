48f3021e780b2b1b1792556c7cc64d111c2ff4aa
uc-pkm <prem.maurya@ucertify.com>
2014-03-25 12:11:02 +0530

    Corrected security issues.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo "This is new line";
 echo "This is new line";
 echo "hi";
-print_r(phpinfo());
+//print_r(phpinfo());  This is commented for security reasons.
 
 Echo "This is the new line added in this file";
 
