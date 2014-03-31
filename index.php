4d0098e1d0b80b83d7a16b665baf3db5c5486703
uc-pkm <prem.maurya@ucertify.com>
2014-03-31 08:39:11 +0530

    Added branch

--- index.php
+++ index.php
@@ -1,29 +1,3 @@
 <?php
-echo "This is new line";
-echo "<hr>";
-echo "hi";
-//print_r(phpinfo());  This is commented for security reasons.
-
-function displayData($data)
-{
-	foreach($data as $i)
-	{
-		echo "THIS IS LINE NO:".$i;
-	}
-}
-
-function displayTable($i){
-	echo "Table of :" . $i . "<br />";;
-	$i = 0;
-	$j = 0;
-	while($j<=10){
-		echo "row => " . $i . "*" . $j . " = " . $i * $j . "<br />";
-		$j++;
-	}
-}
-echo "Removed error";
-displayTable(2);
-echo "added";
-
-echo "Hello this is added today by deepika";
+echo "this is new branch";
 ?>
