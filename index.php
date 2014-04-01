222da14dac31b4a6ba201307ef85666c16fdc769
uc-pkm <prem.maurya@ucertify.com>
2014-04-01 09:22:12 +0530

    Created foreach look on itme

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
-echo "This is initial stage of index.php";
-include "site-config.php";
-echo "this is new branch";
-echo "<pre>";
-print_r($_SERVER);
-echo "This is new line";
-echo "hi";
-print_r(phpinfo());
-echo "this is index file";
-echo "hello this is written for checcking conflict";
-
-echo "this is added in deepika local branch";
-
-ECHO "THIS IS FROM OLD MODE"
+foreach($user as $item)
+{
+	print_r($item);
+}
 ?>
