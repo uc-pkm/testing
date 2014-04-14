c9e7d853962ae1bdbb4f09dbb204900e892908c9
uc-pkm <prem.maurya@ucertify.com>
2014-04-14 09:13:03 +0530

    Optimized index.php

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 echo "THIS IS EXAMPLE OF GIT";
+
 function checkUser($user)
-echo "this is user Auth Module"
-foreach($data as $d)
 {
-	echo "This is data:".$d;
+	echo "this is user Auth Module"
+	foreach($data as $d)
+	{
+		echo "This is data:".$d;
+	}
 }
+
 ?>
