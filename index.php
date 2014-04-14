8d09eb42a548449ce2427fd9537ecb1532888952
uc-pkm <prem.maurya@ucertify.com>
2014-04-14 12:43:16 +0530

    Removed function

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
 <?php
 echo "THIS IS EXAMPLE OF GIT";
-
-function checkUser($user)
-{
-	global $data;
-	echo "this is user Auth Module"
-	foreach($data as $d)
-	{
-		echo "This is data:".$d;
-	}
-}
-
 echo "this is added by deepika";
 ?>
