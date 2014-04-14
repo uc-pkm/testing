31ee85572d712c257c037b090a7ba601d919af66
uc-pkm <prem.maurya@ucertify.com>
2014-04-14 09:14:54 +0530

    Corrected isuse

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 echo "THIS IS EXAMPLE OF GIT";
+
 function checkUser($user)
-echo "this is user Auth Module"
-foreach($data as $d)
 {
-	echo "This is data:".$d;
+	global $data;
+	echo "this is user Auth Module"
+	foreach($data as $d)
+	{
+		echo "This is data:".$d;
+	}
 }
+
 ?>
