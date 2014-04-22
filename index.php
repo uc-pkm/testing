f0277d29f8137295f7497a75e37893acf570c108
Deepika Patel <deepika.patel@ucertify.com>
2014-04-22 09:28:46 +0530

    deleted some lines

--- index.php
+++ index.php
@@ -6,14 +6,4 @@
 echo "Testing I am committing this file from Ubuntu";
 include_once "prepengine-header.php";
 
-function checkUser($user)
-{
-	global $data;
-	echo "this is user Auth Module"
-	foreach($data as $d)
-	{
-		echo "This is data:".$d;
-	}
-}
-
 ?>
